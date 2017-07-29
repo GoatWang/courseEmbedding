241bde155560ae347b3fd716ded565693549137a
jeremy4555 <jeremy4555@yahoo.com.tw>
2017-07-29 14:55:49 +0800

    try query

--- LabelCompany/queryGenerator.py
+++ LabelCompany/queryGenerator.py
@@ -2,12 +2,27 @@ from os import listdir
 import re
 from nltk import word_tokenize
 from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+import string
+
 #files = listdir("companyEmbedding") 
 file = open("companyEmbedding/china_auto_logistics_inc", 'r', encoding='utf8')
 
-queryStr = file.read().replace("\n"," ").replace("\t"," ").replace("\r"," ").replace("  "," ").replace("  "," ").replace("  "," ").replace("  "," ").replace("  "," ").replace("  "," ").replace("  "," ").replace("  "," ").replace("  "," ").replace("  "," ").replace("  "," ").replace("  "," ").replace("  "," ").replace("  "," ").replace("  "," ").replace("  "," ").replace("  "," ")
+queryStr = file.read().replace("\n"," ").replace("\t"," ").replace("\r"," ")
 file.close()
 
+i = 0
+while i < 30 :
+    queryStr = queryStr.replace("  "," ")
+    i+=1
+
+
+
+re_words = re.compile(u"[\u4e00-\u9fa5]+")
+dropStrs = re.findall(re_words, queryStr)
+if len(dropStrs) != 0:
+    for ds in dropStrs:
+        queryStr = queryStr.replace(ds,"")
 
 re_words = re.compile('\{.*\}' )
 dropStrs = re.findall(re_words, queryStr)
@@ -21,17 +36,20 @@ if len(dropStrs) != 0:
     for ds in dropStrs:
         queryStr = queryStr.replace(ds,"")
 
-i = 0
-while i < 10:
-    queryStr = queryStr.replace("  "," ")
-    i += 1
-
-for word in queryStr.split():
-    if b"\xa0" in word.encode() or b'\xee' in word.encode() or "/" in word:
-        queryStr = queryStr.replace(word, "")
+for pun in string.punctuation+"©":
+    queryStr = queryStr.replace(pun, "")
 
 stops = set(stopwords.words('english'))
-queryStr = " ".join([i for i in word_tokenize(queryStr.lower()) if i not in stops])
+lemmer = WordNetLemmatizer()
+
+
+def isfilter(s):
+    return any(not i.encode('utf-8').isalpha() for i in s)
+
+queryStr = queryStr.lower().strip()
+queryStr.replace('®','')
+wordArr = [lemmer.lemmatize(i) for i in queryStr.split() if ((i not in stops) and (not isfilter(i))) ]
+paramStr = ' '.join(wordArr)
 
 
 paramStr =""
