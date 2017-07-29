730f5285370688d3654d80e11789289d82b300ce
jeremy4555 <jeremy4555@yahoo.com.tw>
2017-07-29 13:56:10 +0800

    change run query param

--- LabelCompany/queryGenerator.py
+++ LabelCompany/queryGenerator.py
@@ -1,15 +1,51 @@
 from os import listdir
-
+import re
 #files = listdir("companyEmbedding") 
 file = open("companyEmbedding/china_auto_logistics_inc", 'r', encoding='utf8')
 
-queryStr = file.read().replace("\n"," ").replace("\t"," ").replace("\r"," ").replace("  "," ").replace("  "," ").replace("  "," ").replace("  "," ").replace("  "," ")
+queryStr = file.read().replace("\n"," ").replace("\t"," ").replace("\r"," ").replace("  "," ").replace("  "," ").replace("  "," ").replace("  "," ").replace("  "," ").replace("  "," ").replace("  "," ").replace("  "," ").replace("  "," ").replace("  "," ").replace("  "," ").replace("  "," ").replace("  "," ").replace("  "," ").replace("  "," ").replace("  "," ").replace("  "," ")
 file.close()
+
+
+re_words = re.compile('\{.*\}' )
+dropStrs = re.findall(re_words, queryStr)
+if len(dropStrs) != 0:
+    for ds in dropStrs:
+        queryStr = queryStr.replace(ds,"")
+
+re_words = re.compile('[\d]+')
+dropStrs = re.findall(re_words, queryStr)
+if len(dropStrs) != 0:
+    for ds in dropStrs:
+        queryStr = queryStr.replace(ds,"")
+
+i = 0
+while i < 10:
+    queryStr = queryStr.replace("  "," ")
+    i += 1
+
+for word in queryStr.split():
+    if b"\xa0" in word.encode() or b'\xee' in word.encode() or "/" in word:
+        queryStr = queryStr.replace(word, "")
+
 
 paramStr =""
 
 paramStr += "<parameters>\n"
-paramStr += "<query>#combine("+queryStr+")</query>\n" 
+
+paramStr += "<index>index/company</index>\n"
+
+paramStr += "<query>\n"
+
+paramStr += "<number>1</number>\n"
+paramStr += "<text>#combine("+queryStr+")</text>\n"
+paramStr += "<baseline>method:tfidf</baseline>\n"
+
+paramStr += "</query>\n"
+paramStr += "<count>1000</count>\n"
+paramStr += "</query>\n"
+
+
 paramStr += "</parameters>"
 
 
